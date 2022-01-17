3ae7286eee9254d31d8b48dbbff6f62b1638a240
Wilson Júnior <willnapolitano21@gmail.com>
2022-01-17 17:39:18 -0300

    Add the position X and Y operators to the randomPosition() function

--- src/js/head.js
+++ src/js/head.js
@@ -31,8 +31,8 @@ function randomPosition(){
     let positionX = Math.floor(Math.random() * width) - 90
     let positionY = Math.floor(Math.random() * height) - 90
 
-positionX = positionX < 0 ? 0 : positionX
-positionY = positionY < 0 ? 0 : positionY
+    positionX = positionX < 0 ? 0 : positionX
+    positionY = positionY < 0 ? 0 : positionY
 
 console.log(positionX, positionY)
 
