260197bb3b012b1e17b03d3b879913cdf2124b75
Wilson Júnior <willnapolitano21@gmail.com>
2022-01-18 17:07:46 -0300

    Create a level select button and a start game function

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+let start = document.querySelector("#start")
+let level;
+
+
+start.addEventListener("click", function(){
+    startGame()
+})
+
+function startGame(){
+    level = document.querySelector("#level").value
+    if(level === ''){
+        alert("Select a level to start the game")
+        return false;
+    }
+    alert(level)
+}
